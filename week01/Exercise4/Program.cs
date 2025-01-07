fe83e988261b5eb8e534130eee820256d080103f
Christian Martinez <140223696+cmart1109@users.noreply.github.com>
2025-01-07 13:57:27 -0600

    Exercise 4

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -1,9 +1,43 @@
 using System;
-
+using System.Globalization;
+using System.Collections.Generic;
+using System.Linq;
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+    List<int> numberLists = new List<int>(); 
+    string numbers;
+    int numberToList;
+        Console.WriteLine("Enter a list of numbers, type 0 when Finished");
+        do
+        {
+        Console.WriteLine("Enter number: ");
+        numbers = Console.ReadLine();
+        numberToList = int.Parse(numbers);
+        if (numberToList != 0)
+        {
+        numberLists.Add(numberToList);
+        }
+        else {
+
+        }
+
+        } while (numberToList != 0);
+
+        foreach (int numberList in numberLists)
+        {
+            Console.WriteLine(numberList);
+        }
+        Console.WriteLine($"The Amount of the numbers is: {numberLists.Count}");
+        Console.WriteLine($"The Sum of the numbers is: {numberLists.Sum()}");
+        Console.WriteLine($"The Average of the numbers is: {numberLists.Average()}");
+        Console.WriteLine($"The Maximun number on the list is: {numberLists.Max()}");
+        numberLists.Sort();
+        Console.WriteLine("Here is the List Sorted:");
+         foreach (int numberList in numberLists)
+        {
+            Console.WriteLine(numberList);
+        }
     }
 }
