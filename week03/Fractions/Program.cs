bbcdefbe237137437d011dd0ee9b750f3ba59df4
Christian Martinez <140223696+cmart1109@users.noreply.github.com>
2025-01-23 08:38:57 -0600

    fractions

--- week03/Fractions/Program.cs
+++ week03/Fractions/Program.cs
@@ -1,9 +1,16 @@
 using System;
+using System.Runtime.CompilerServices;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Fractions Project.");
+        Fraction f1 = new Fraction();
+        Console.WriteLine($"{f1.GetFractionString()}");
+        f1.SetTop(1);
+        f1.GetBottom();
+        f1.SetBottom(5);
+        f1.GetBottom();
+        f1.GetDecimalValue();
     }
 }
