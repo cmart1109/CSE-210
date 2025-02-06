98663f305f1d4faa5a3f41a64f3d31976ce9404a
Christian Martinez <140223696+cmart1109@users.noreply.github.com>
2025-02-05 21:57:11 -0600

    mind2

---
+++ week05/Mindfulness/BreathingActivity.cs
@@ -0,0 +1,13 @@
+using System.Reflection.PortableExecutable;
+
+public class BreathingActivity : Activity
+{
+    public BreathingActivity()
+    {
+
+    }
+    public void Run()
+    {
+        Console.WriteLine("Let's Breathe!");
+    }
+}
---
+++ week05/Mindfulness/ListingActivity.cs
@@ -0,0 +1,22 @@
+public class ListingActivity : Activity
+{
+    private int _count;
+    List<string> _prompts = new List<string>();
+
+    public ListingActivity()
+    {
+
+    }
+    public void Run()
+    {
+
+    }
+    public void GetRandomPrompt()
+    {
+
+    }
+    public List<string> GetListFromUser()
+    {
+        return _prompts;
+    }
+}
